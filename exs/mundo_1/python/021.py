8d60b47909552040199df9e8969a54eb43022eed
Matheus Felipe <50463866+matheusfelipeog@users.noreply.github.com>
2020-12-31 05:49:23 -0300

    Rename all files to the correct pattern

--- exs/mundo_1/python/021.py
+++ exs/mundo_1/python/021.py
@@ -8,6 +8,6 @@ Resolução do problema:
 import pygame
 pygame.mixer.init()
 pygame.init()
-pygame.mixer.music.load('ex021.mp3')
+pygame.mixer.music.load('021.mp3')
 pygame.mixer.music.play()
 pygame.event.wait()
