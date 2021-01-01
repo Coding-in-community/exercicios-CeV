59bb44c103d1885c01641a066ee562cf4b9162d0
Kevin <52904595+KevinAp-5@users.noreply.github.com>
2021-01-01 20:17:30 -0300

    Update exs/mundo_1/java/001.java
    removed useless comment
    
    Co-authored-by: Matheus Felipe <50463866+matheusfelipeog@users.noreply.github.com>

--- exs/mundo_1/java/001.java
+++ exs/mundo_1/java/001.java
@@ -7,6 +7,6 @@ Resolução do problema:
 
 class Main{
     public static void main(String[] args){
-        System.out.println("Hello, world"); // Printa "Hello, world"
+        System.out.println("Hello, world");
     }
 }
