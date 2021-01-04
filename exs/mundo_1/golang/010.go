7e4ef315ac712607ea656f319d33dc1eb86e906e
Patrick D. Reis <33698647+Typhz@users.noreply.github.com>
2021-01-04 16:37:00 -0300

    Adicionando novos desafios GoLang

---
+++ exs/mundo_1/golang/010.go
@@ -0,0 +1,22 @@
+/*
+	Desafio: 010
+	Problema: Crie um programa que leia quanto dinheiro uma pessoa tem
+    na carteira e mostre quantos dólares ela pode comprar.
+
+*/
+
+package main
+import (
+	"fmt"
+)
+
+func main() {
+	// var real float64
+	var real float64
+	fmt.Print("Informe um valor: ")
+	fmt.Scanln(&real)
+	dol := 5.24
+	resultado := float64(real) / float64(dol)
+	fmt.Printf("Você pode comprar $%.2f", resultado)
+
+}
