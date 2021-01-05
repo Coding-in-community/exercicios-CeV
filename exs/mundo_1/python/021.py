fb7d1479dcc2b20dd75df0f72226c8066e60e75d
Matheus Felipe <50463866+matheusfelipeog@users.noreply.github.com>
2021-01-05 14:28:14 -0300

    [FIX] Correção na execução de áudio do exercício 21 em Python

--- exs/mundo_1/python/021.py
+++ exs/mundo_1/python/021.py
@@ -6,8 +6,26 @@ Problema: Faça um programa em Python que abra e reproduza o áudio de um arquiv
 Resolução do problema:
 """
 import pygame
-pygame.mixer.init()
+from time import sleep
+
+audio = '../assets/audio/021.mp3'
+
 pygame.init()
-pygame.mixer.music.load('021.mp3')
+pygame.mixer.init()
+pygame.mixer.music.load(audio)
 pygame.mixer.music.play()
-pygame.event.wait()
+
+# Observação:
+# Em uma versão antiga o método "pygame.event.wait()" era utilizado para manter
+# o terminal em execução enquanto a música estive-se tocando. Na versão recente
+# não é mais possível usa-lo dessa forma. Existe diversas alternativas para ter
+# o mesmo resultado, a escolhida foi a presente abaixo por ser a mais simples.
+# 
+# "pygame.mixer.music.get_busy()" verifica se existe algum áudio tocando.
+# "while" mantém o áudio tocano e faz com que a verificação repita ENQUANTO o 
+# áudio estiver tocando. Quando o áudio acabar o programa continua e finaliza.
+while pygame.mixer.music.get_busy():
+    print('A música está tocando...')
+    sleep(1)
+
+print('A música acabou.')
