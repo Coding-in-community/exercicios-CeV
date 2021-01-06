750f28e196e65e69ffd5c6f97b3a8d951f66cd9a
QuatroQuatros <guicossalter@gmail.com>
2021-01-05 21:13:11 -0300

    [Novo] exercicio 012 em Go 1.8.

--- exs/mundo_1/golang/011.go
+++ exs/mundo_1/golang/011.go
@@ -1,9 +1,11 @@
 /*
 Desafio 011
+
 Problema: Faça um programa que leia a largura e a altura de uma
           parede em metros, calcule a sua área e a quantidade de
           tinta necessária para pintá-la, sabendo que cada litro
           de tinta pinta uma área de 2 metros quadrados.
+
 Resolução do problema:
 */
 
